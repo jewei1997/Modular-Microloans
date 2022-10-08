3b60c78456b430d8ccdfa4edf0fd1a45a6f2c4df
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2022-10-08 07:23:52 -0500

    fix approve check

--- src/PreCommitManager.sol
+++ src/PreCommitManager.sol
@@ -30,52 +30,90 @@ contract PreCommitManager {
     uint256 lastCommitId;
 
     event ProjectCreated(uint256 projectId, address creator);
-    event FundsPulledForProject(uint256 projectId, address creator, uint256 totalAmount);
+    event FundsRedeemedForProject(
+        uint256 projectId,
+        address creator,
+        uint256 totalAmount
+    );
     event RedeemFailed(uint256 projectId, uint256 commitId, uint256 amount);
     event RedeemSucceeded(uint256 projectId, uint256 commitId, uint256 amount);
-    event CommitCreated(uint256 commitId, uint256 projectId, address commiter, address erc20Token, uint256 amount, uint256 expiry);
+    event CommitCreated(
+        uint256 commitId,
+        uint256 projectId,
+        address commiter,
+        address erc20Token,
+        uint256 amount,
+        uint256 expiry
+    );
     event CommitWithdrawn(uint256 commitId, address commiter);
-
-    constructor() {}
 
     function createProject(address projectAcceptedAsset) public {
         lastProjectId++;
-        projects[lastProjectId] = Project({receiver: msg.sender, asset: projectAcceptedAsset});
+        projects[lastProjectId] = Project({
+            receiver: msg.sender,
+            asset: projectAcceptedAsset
+        });
 
         emit ProjectCreated(lastProjectId, msg.sender);
     }
 
     function redeem(uint256 projectId, uint256[] memory commitIds) public {
-        require(projects[projectId].receiver == msg.sender, "Only project creator can pull funds for the project");
+        require(
+            projects[projectId].receiver == msg.sender,
+            "Only project creator can pull funds for the project"
+        );
 
         uint256 totalAmount = 0;
         for (uint256 i = 0; i < commitIds.length; i++) {
             Commit memory commit_ = commits[commitIds[i]];
             Project memory project = projects[commit_.projectId];
-            require(commit_.projectId == projectId, "Commit does not belong to the project");
-            require(project.receiver == msg.sender, "Commit does not belong to the project");
+            require(
+                commit_.projectId == projectId,
+                "Commit does not belong to the project"
+            );
+            require(
+                project.receiver == msg.sender,
+                "Commit does not belong to the project"
+            );
             require(commit_.expiry > block.timestamp, "Commit expired");
 
-            bool success = IERC20(commit_.erc20Token).transferFrom(commit_.commiter, project.receiver, commit_.amount);
+            bool success = IERC20(commit_.erc20Token).transferFrom(
+                commit_.commiter,
+                project.receiver,
+                commit_.amount
+            );
             if (success) {
                 totalAmount += commit_.amount;
                 delete commits[commitIds[i]];
-                emit RedeemSucceeded(projectId, commit_.commitId, commit_.amount);
+                emit RedeemSucceeded(
+                    projectId,
+                    commit_.commitId,
+                    commit_.amount
+                );
             } else {
                 emit RedeemFailed(projectId, commit_.commitId, commit_.amount);
             }
         }
-        
-        emit FundsPulledForProject(projectId, msg.sender, totalAmount);
+
+        emit FundsRedeemedForProject(projectId, msg.sender, totalAmount);
     }
 
-    function commit(uint256 projectId, uint256 amount, uint256 deadline) public {
+    function commit(
+        uint256 projectId,
+        uint256 amount,
+        uint256 deadline
+    ) public {
         require(amount > 0, "Amount must be greater than 0");
         require(deadline > block.timestamp, "Deadline must be in the future");
 
         // if token is not projectAcceptedAsset, allow approve and swap upon pulling
         address asset = projects[projectId].asset;
-        IERC20(asset).safeApprove(address(this), amount);        
+        // check for allowance
+        require(
+            IERC20(asset).allowance(msg.sender, address(this)) >= amount,
+            "address has not approved contract to use its funds"
+        );
+
         // increment commit data
         lastCommitId++;
         Commit memory commitData = Commit({
@@ -88,11 +126,21 @@ contract PreCommitManager {
         });
         commits[lastCommitId] = commitData;
 
-        emit CommitCreated(lastCommitId, projectId, msg.sender, asset, amount, deadline);
+        emit CommitCreated(
+            lastCommitId,
+            projectId,
+            msg.sender,
+            asset,
+            amount,
+            deadline
+        );
     }
 
     function withdrawCommit(uint256 commitId) public {
-        require(commits[commitId].commiter == msg.sender, "Only commiter can withdraw");
+        require(
+            commits[commitId].commiter == msg.sender,
+            "Only commiter can withdraw"
+        );
         delete commits[commitId];
 
         emit CommitWithdrawn(commitId, msg.sender);

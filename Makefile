fdd1f2545ca96a18aa5ca8b39167b1b507d5491f
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2022-10-08 09:58:00 -0500

    update deploy script to populate contract

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@
 # (-include to ignore error if it does not exist)
 -include .env
 
-deploy : forge script script/Deploy.s.sol:Deploy --rpc-url ${ETH_RPC_URL} --private-key ${ETH_KEYSTORE} --legacy --broadcast -vvvv
+deploy :; forge script script/Deploy.s.sol:Deploy --rpc-url ${ETH_RPC_URL} --private-keys ${ETH_KEYSTORE} --private-keys ${ADDRESS_USER} --private-keys ${ADDRESS_USER_2} --legacy --broadcast -vvvv
--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,15 +4,50 @@ pragma solidity 0.8.15;
 import "forge-std/Script.sol";
 
 import {PreCommitManager} from "src/PreCommitManager.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
 
 contract Deploy is Script {
+    // global vars
+    PreCommitManager preComManager;
+    ERC20Mock usdc;
+
     function run() external {
-        vm.startBroadcast();
-
-        PreCommitManager preComManager = new PreCommitManager();
-
+        uint256 admin = vm.envUint("ETH_KEYSTORE");
+        vm.startBroadcast(admin);
+        preComManager = new PreCommitManager();
         console.log("PreCommitManager: ", address(preComManager));
 
+        // setup for populating the graph
+
+        // deploy mock USDC
+        address initialAccount = msg.sender;
+        uint256 initialAmount = 100e18; // this mocked USDC have 18 decimals
+        usdc = new ERC20Mock("USDC Mumbai", "USDC", initialAccount, 100e18);
+
+        // allowance so precommit manager to use addresss usdc funds
+        usdc.approve(address(preComManager), initialAmount);
+        vm.stopBroadcast();
+
+        // user 1 creates project 1
+        uint256 user1PrivateKey = vm.envUint("ADDRESS_USER");
+        vm.startBroadcast(user1PrivateKey);
+        // create project with id 1
+        preComManager.createProject(address(usdc));
+        vm.stopBroadcast();
+
+        // user 2 commits to project 1
+        uint256 user2PrivateKey = vm.envUint("ADDRESS_USER_2");
+        vm.startBroadcast(user2PrivateKey);
+        // add funds to user2
+        uint256 user2AmountToCommit = 50e18;
+        usdc.mint(msg.sender, user2AmountToCommit);
+        usdc.approve(address(preComManager), user2AmountToCommit);
+        // commit to the project
+        preComManager.commit({
+            projectId: 1,
+            amount: user2AmountToCommit,
+            deadline: block.timestamp + 1 weeks
+        });
         vm.stopBroadcast();
     }
 }

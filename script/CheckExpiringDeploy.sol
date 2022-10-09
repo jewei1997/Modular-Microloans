baeaf7c0b48a52c8165c74e2973093b47f45a7b6
Jarku Tang <jarku@syndicate.io>
2022-10-08 21:44:13 -0500

    Update deploy script

--- script/CheckExpiringDeploy.sol
+++ script/CheckExpiringDeploy.sol
@@ -3,23 +3,27 @@ pragma solidity 0.8.15;
 
 import "forge-std/Script.sol";
 
+import {preCommitManager} from "src/preCommitManager.sol";
 import {CheckExpiring} from "src/CheckExpiring.sol";
-import {PreCommitManager} from "src/PreCommitManager.sol";
+import {CheckExpired} from "src/CheckExpired.sol";
 
-contract CheckExpiringDeploy is Script {
+contract DeployAutomaticChecks is Script {
     // global vars
-    PreCommitManager preComManager;
-    CheckExpiring cEx;
+    preCommitManager preCommitManager;
+    CheckExpiring checkExpiring;
+    CheckExpired checkExpired;
 
     function run() external {
         uint256 admin = vm.envUint("ETH_KEYSTORE");
         vm.startBroadcast(admin);
-        preComManager = PreCommitManager(
+        preCommitManager = preCommitManager(
             // UPDATE ADDRESS
             0x80F7b90C88A80eE1e60e74Ce0c6207DfcA2E182C
         );
-        cEx = new CheckExpired(preComManager);
-        console.log("CheckExpired: ", address(cEx));
+        checkExpiring = new CheckExpiring(preCommitManager);
+        console.log("CheckExpiring: ", address(checkExpiring));
+        checkExpired = new CheckExpired(preCommitManager);
+        console.log("CheckExpired: ", address(checkExpired));
         vm.stopBroadcast();
     }
 }
